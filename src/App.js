9703f65769de7a50928dadcd6b369d5d884f3b27
onomlight <pass041@nate.com>
2023-02-05 16:55:20 +0900

    데이터 가지고오기 //19 다시보기 필요

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
-import { useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import { Fragment, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
 import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
-import { uiActions } from './store/ui-slice'
+import Notification from './components/UI/Notification'
+import { sendCartData, fetchCartData } from './store/cart-actions'
+
+let isInitial = true
 
 function App() {
+  const dispatch = useDispatch()
   const showCart = useSelector((state) => state.ui.cartIsVisible)
   const cart = useSelector((state) => state.cart)
+  const notification = useSelector((state) => state.ui.notification)
+
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatchEvent(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending..',
-          message: 'Sending cart data!',
-        }),
-      )
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/cart.json',
-        {
-          // firebase 연결 /// 해제함
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        },
-      ) // 백엔드 연결하기
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.')
-      }
-      const responseData = await response.json()
+    dispatch(fetchCartData())
+  }, [dispatch])
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false
+      return
     }
-  }, [cart])
-  useEffect()
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart))
+    }
+  }, [cart, dispatch])
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   )
 }
 
